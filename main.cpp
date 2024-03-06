21a0eff4b5baf093c4947cbcff7be671162a4075
student <default@bscse.uiu.ac.bd>
2024-03-06 09:53:52 +0600

    feat: add main function

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// write an add method: sourav
+
+//write a subtract method: badhan
+
+//write a division method: adnan
+
+//write a multiply method: redoan
+
+int main(){
+
+}
